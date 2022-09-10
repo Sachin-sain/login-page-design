01c8f3714c1b57b7e0a7d37bceaa39f8d86af77e
Sun <sainisachin7413@gmai.com>
2022-09-10 17:38:50 +0530

    login page complete code

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+import 'package:loginpage/register.dart';
+import 'package:loginpage/login.dart';
+void main(){
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+         initialRoute: 'login',
+        routes: {
+          'login':(context)=>MyLogin(),
+          'register':(context)=>MyRegister(),
+
+        },
+  )
+  );
+}
